eb067c855d0ebd0c94a73c5031c9c42c0c9cf030
lgn <legionidk@internet.ru>
2025-12-15 06:11:17 +0300

    updated frontend w/placeholders

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { getWeaponsData, getWeaponSkins } from "./api.js";
 import { renderNavigationButtons, renderSkins } from "./dom.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    getWeaponsData().then((data) => {
-        const firstWeaponUuid = renderNavigationButtons(data);
+// document.addEventListener("DOMContentLoaded", () => {
+//     getWeaponsData().then((data) => {
+//         const firstWeaponUuid = renderNavigationButtons(data);
 
-        getWeaponSkins(firstWeaponUuid).then((skins) => {
-            renderSkins(skins);
-        });
-    });
-});
+//         getWeaponSkins(firstWeaponUuid).then((skins) => {
+//             renderSkins(skins);
+//         });
+//     });
+// });
