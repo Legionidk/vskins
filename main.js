7f82a410da27840fe6ee14dcf484c3a88f5724bf
lgnidk <legionidk@internet.ru>
2026-03-21 23:12:24 +0300

    weapons page main.js

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+import { createCategory, createWeaponCard } from "./src/js/dom";
+import { getWeapons } from "./src/js/api";
+
+const weaponsData = getWeapons();
+
+weaponsData.then((data) => {
+    for (const [uuid, weaponData] of Object.entries(data)) {
+        let category = document.body.querySelector(
+            `div[data-category-uuid="${weaponData.categoryId}"]`,
+        );
+        if (category === null) {
+            category = createCategory(
+                weaponData.categoryText,
+                weaponData.categoryId,
+            );
+            document.body.querySelector("main").append(category);
+        }
+
+        category
+            .querySelector("#cards-wrapper")
+            .append(createWeaponCard(uuid, weaponData));
+    }
+});
+
 document.body.addEventListener("click", (e) => {
     if (e.target.closest("#card") !== null) {
         document.body
