db06c052073121bc111c2a6b028b0c080ea1bffb
lgnidk <legionidk@internet.ru>
2026-04-20 12:39:41 +0300

    updated app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,13 @@
 import Header from "@/components/header";
+import MainGrid from "@/components/maingrid";
+import Footer from "@/components/footer";
 
 export default function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <MainGrid />
+            <Footer />
+        </>
     );
 }
