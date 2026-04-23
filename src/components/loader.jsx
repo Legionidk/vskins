50ff4315718919e14bcfa3ac60400df2f96f1e27
lgnidk <legionidk@internet.ru>
2026-04-23 13:37:36 +0300

    loader

---
+++ src/components/loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+    return (
+        <div
+            className="rounded-[50%] border-4 border-[#292727] border-b-[#FF4248] animate-spin h-[40px] w-[40px]"
+            id="load-spinner"
+        />
+    );
+}
