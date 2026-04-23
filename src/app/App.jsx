bcb77e3000ba13826d09adf8cbf8e3b3a9fe2f50
lgnidk <legionidk@internet.ru>
2026-04-23 13:37:31 +0300

    cards render

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from "react";
 import Header from "@/components/header";
 import Main from "@/components/main";
+import Category from "@/components/category";
+import Loader from "@/components/loader";
 import Footer from "@/components/footer";
+import getWeapons from "@/services/weaponApi";
 
 export default function App() {
+    const [weapons, setWeapons] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getWeapons().then((data) => {
+            setWeapons(data);
+            setLoading(false);
+        });
+    }, []);
+
     return (
         <>
             <Header />
-            <Main />
+            <Main>
+                {loading ? (
+                    <Loader />
+                ) : (
+                    weapons.map((weaponCategory) => {
+                        return (
+                            <Category
+                                key={weaponCategory.categoryId}
+                                categoryId={weaponCategory.categoryId}
+                                categoryName={weaponCategory.categoryName}
+                                weaponsData={weaponCategory.weapons}
+                            />
+                        );
+                    })
+                )}
+            </Main>
             <Footer />
         </>
     );
