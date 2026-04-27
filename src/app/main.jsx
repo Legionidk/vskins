c001d9681675ac8744b6cf408c10267406387842
lgnidk <legionidk@internet.ru>
2026-04-27 15:37:31 +0300

    fix shitty code

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,4 +2,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "@/styles/global.css";
 
-ReactDOM.createRoot(document.body).render(<App />);
+ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
