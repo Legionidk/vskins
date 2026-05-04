88bfdb3597179c48e66bd67241ad298755c6e9a0
lgnidk <legionidk@internet.ru>
2026-05-04 16:04:03 +0300

    fix for rigby

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
             />
 
             {rigbyModal && (
-                <Rigby rigbyCloseFunc={() => setRigbyModal(false)} />
+                <RigbyModal rigbyCloseFunc={() => setRigbyModal(false)} />
             )}
         </>
     );
