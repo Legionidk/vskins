2fe101471412a3a106c2df3c5aba39a05f771bb4
lgnidk <legionidk@internet.ru>
2026-06-27 17:23:58 +0300

    footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -2,49 +2,36 @@ import githubLogo from "@/assets/github.svg";
 import yoPng from "@/assets/yo.png";
 
 export default function Footer() {
-    const baseClasses =
-        "flex flex-col justify-center items-center bg-[#111111] border-t-2 border-[#333333] px-[50px] py-[10px] w-full text-lg font-light mt-auto";
-    const responsiveClasses =
-        "sm:flex-row sm:flex-row-reverse sm:justify-between xl:px-[200px]";
-
     return (
-        <footer className={`${baseClasses} ${responsiveClasses}`}>
-            <div className="flex gap-2" id="footer-buttons">
-                <a
-                    href="https://github.com/Legionidk/vskins"
-                    id="github-button"
-                    target="_blank"
-                >
-                    <img
-                        src={githubLogo}
-                        alt="Github logo"
-                        className="rounded-[8px] p-2 hover:bg-[#292727]"
-                        id="github-logo"
-                    />
-                </a>
-
-                <div
-                    className="cursor-pointer"
-                    id="yo-button"
-                >
-                    <img
-                        src={yoPng}
-                        alt="yo image"
-                        className="rounded-[8px] p-2 hover:bg-[#292727]"
-                        id="github-logo"
-                    />
-                </div>
-            </div>
-
-            <p className="text-lg xl:text-2xl" id="footer-text">
+        <footer className="w-full h-[60px] flex items-center justify-between px-[20px] mt-auto bg-[#111111] border-t-1 border-[#323232] text-[16px] font-light">
+            <p>
                 Data provided by
                 <a
-                    href="https://valorant-api.com"
-                    className="text-[#FF4248] hover:underline ml-[5px]"
+                    href="https://valorant-api.com/"
+                    className="text-[#FF4248] ml-[5px] hover:underline"
                 >
                     valorant-api.com
                 </a>
             </p>
+
+            <div
+                className="flex items-center gap-[10px]"
+                id="buttons-wrapper"
+            >
+                <a
+                    className="w-[40px] h-[40px] rounded-[8px] p-[8px] hover:bg-[#323232]"
+                    href="https://github.com/Legionidk/vskins"
+                >
+                    <img src={githubLogo} alt="GitHub logo" />
+                </a>
+
+                <button
+                    className="w-[40px] h-[40px] rounded-[8px] p-[8px] hover:bg-[#323232]"
+                    id="rigby-button"
+                >
+                    <img src={yoPng} alt="Rigby icon" />
+                </button>
+            </div>
         </footer>
     );
 }
--- src/pages/weapons.jsx
+++ src/pages/weapons.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
+
 import Header from "../components/layout/header";
+import Footer from "../components/layout/footer";
 
 export default function WeaponsPage() {
     return (
@@ -7,6 +9,7 @@ export default function WeaponsPage() {
             <Header />
             <p>It's weapons page</p>
             <Link to="/agents">Agents page</Link>
+            <Footer />
         </>
     );
 }
