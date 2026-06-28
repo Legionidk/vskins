4b6aaf50141d512d801e59cab36d3ca21ece6d77
lgnidk <legionidk@internet.ru>
2026-06-28 16:31:38 +0300

    header animation

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,19 +1,27 @@
 import { createPortal } from "react-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import logo from "@/assets/asgard.svg";
 
-function Burger({ buttonsData = [], closeFunc }) {
+function Burger({ buttonsData = [], closeFunc, unmountFunc, isOpened }) {
     return createPortal(
         <div
-            className="absolute top-[81px] inset-x-0 bottom-0 bg-black/50 backdrop-blur-xs"
+            className={`absolute top-[81px] inset-x-0 bottom-0 bg-black/50 backdrop-blur-xs transition-opacity duration-50 ease-in-out ${isOpened ? "opacity-100" : "opacity-0"}`}
             id="burger-menu-wrapper"
             onClick={closeFunc}
+            onTransitionEnd={() => {
+                if (!isOpened) {
+                    unmountFunc();
+                }
+            }}
         >
             <div
                 className="w-full h-fit flex flex-col items-end gap-[10px] bg-[#111111] p-[20px]"
                 id="burger-menu"
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
             >
                 {buttonsData.length === 0 ? (
                     <p className="text-[18px] text-[#b8b8b8] font-light self-center">
@@ -29,20 +37,44 @@ function Burger({ buttonsData = [], closeFunc }) {
 }
 
 export default function Header() {
-    const [isBurger, setBurger] = useState(false);
+    const [isBurgerOpened, setBurgerOpened] = useState(false);
+    const [isBurgerMounted, setBurgerMounted] = useState(false);
 
     const burgerButtonFunc = () => {
-        setBurger(!isBurger);
+        if (isBurgerOpened) {
+            setBurgerOpened(false);
+            return;
+        }
+
+        setBurgerMounted(true);
     };
+
+    useEffect(() => {
+        if (isBurgerMounted) {
+            requestAnimationFrame(() => {
+                setBurgerOpened(true);
+            });
+        }
+    }, [isBurgerMounted]);
 
     return (
         <>
-            {isBurger && <Burger closeFunc={burgerButtonFunc} />}
+            {isBurgerMounted && (
+                <Burger
+                    isOpened={isBurgerOpened}
+                    closeFunc={() => {
+                        setBurgerOpened(false);
+                    }}
+                    unmountFunc={() => {
+                        setBurgerMounted(false);
+                    }}
+                />
+            )}
 
             <header className="w-full h-[80px] flex items-center justify-between px-[20px] bg-[#111111] border-b-1 border-[#323232]">
                 <img src={logo} alt="Asgard logo" className="h-[30px]" />
                 <button id="burger-button" onClick={burgerButtonFunc}>
-                    {isBurger ? (
+                    {isBurgerOpened ? (
                         <XMarkIcon className="h-[24px]" />
                     ) : (
                         <Bars3Icon className="h-[24px]" />
