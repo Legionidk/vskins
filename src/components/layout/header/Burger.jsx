6c38892594869ea994df49c87af4e79c9508cbcd
lgnidk <legionidk@internet.ru>
2026-06-30 14:42:55 +0300

    updated burger

--- src/components/layout/header/Burger.jsx
+++ src/components/layout/header/Burger.jsx
@@ -1,24 +1,64 @@
+import { createPortal } from "react-dom";
+import { useState, useEffect } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
 import HeaderButton from "./HeaderButton";
 
-export default function Burger({ buttonsData = [] }) {
-    return (
+export default function Burger({ buttonsData = [], closeFunc }) {
+    const [isVisible, setVisible] = useState(false);
+
+    useEffect(() => {
+        setVisible(true);
+    }, []);
+
+    return createPortal(
         <div
-            className="w-full h-fit flex flex-col items-end gap-[10px] p-[20px] border-t-1 border-[#323232] transition-all duration-50 ease-in-out"
+            className={`fixed top-0 w-full min-h-dvh flex flex-col items-center bg-[#111111] transition-all duration-50 ease-in-out ${isVisible ? "translate-y-0 opacity-100" : "-translate-y-5 opacity-0"}`}
             id="burger-menu"
+            onTransitionEnd={() => {
+                if (!isVisible) {
+                    closeFunc();
+                }
+            }}
         >
-            {buttonsData.length === 0 ? (
-                <p className="text-[18px] text-[#b8b8b8] font-light self-center">
-                    There is no elements to render :(
+            <div
+                className="w-full h-[80px] flex items-center justify-between px-[20px] border-b-1 border-[#323232]"
+                id="header"
+            >
+                <p className="uppercase tracking-widest font-medium text-[20px]">
+                    Menu
                 </p>
-            ) : (
-                buttonsData.map((button) => (
-                    <HeaderButton
-                        text={button.text}
-                        status={button.status}
-                        link={button.link}
-                    />
-                ))
-            )}
-        </div>
+                <button
+                    className="size-[24px]"
+                    id="close-button"
+                    onClick={() => {
+                        setVisible(false);
+                    }}
+                >
+                    <XMarkIcon />
+                </button>
+            </div>
+
+            <div
+                className="size-full flex flex-col items-end gap-[10px] p-[20px]"
+                id="buttons-wrapper"
+            >
+                {buttonsData.length === 0 ? (
+                    <p className="text-[18px] text-[#b8b8b8] font-light self-center">
+                        There is no elements to render :(
+                    </p>
+                ) : (
+                    buttonsData.map((button) => (
+                        <HeaderButton
+                            text={button.text}
+                            status={button.status}
+                            link={button.link}
+                            key={button.link}
+                        />
+                    ))
+                )}
+            </div>
+        </div>,
+        document.querySelector("#modal-root"),
     );
 }
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -5,15 +5,21 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Burger from "./Burger";
 import logo from "@/assets/asgard.svg";
 
-export default function Header({ buttonsData=[] }) {
+export default function Header({ buttonsData = [] }) {
     const [isBurgerOpened, setBurgerOpened] = useState(false);
 
     return (
-        <header className="fixed top-0 w-full h-fit flex flex-col items-center bg-[#111111] border-b-1 border-[#323232]">
-            <div
-                className="w-full h-[80px] flex items-center justify-between px-[20px]"
-                id="header-content-wrapper"
-            >
+        <>
+            {isBurgerOpened && (
+                <Burger
+                    buttonsData={buttonsData}
+                    closeFunc={() => {
+                        setBurgerOpened(false);
+                    }}
+                />
+            )}
+
+            <header className="fixed top-0 w-full h-[80px] flex justify-between items-center px-[20px] bg-[#111111] border-b-1 border-[#323232]">
                 <img src={logo} alt="Asgard logo" className="h-[30px]" />
                 <button
                     id="burger-button"
@@ -21,19 +27,9 @@ export default function Header({ buttonsData=[] }) {
                         setBurgerOpened(!isBurgerOpened);
                     }}
                 >
-                    {isBurgerOpened ? (
-                        <XMarkIcon className="size-[24px]" />
-                    ) : (
-                        <Bars3Icon className="size-[24px]" />
-                    )}
+                    <Bars3Icon className="size-[24px]" />
                 </button>
-            </div>
-
-            <div
-                className={`w-full overflow-hidden transition-all duration-50 ease-in-out ${isBurgerOpened ? "max-h-screen" : "max-h-0"}`}
-            >
-                <Burger buttonsData={buttonsData} />
-            </div>
-        </header>
+            </header>
+        </>
     );
 }
