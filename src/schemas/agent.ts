ca5d9863c5ec7c6927de4466e34bacf3d46e39f2
lgnidk <legionidk@internet.ru>
2026-08-29 14:44:45 +0300

    agent api schema

---
+++ src/schemas/agent.ts
@@ -0,0 +1,28 @@
+import * as v from "valibot";
+
+const AbilitySchema = v.object({
+    slot: v.string(),
+    displayName: v.string(),
+    description: v.string(),
+    displayIcon: v.string(),
+});
+
+export const AgentSchema = v.object({
+    uuid: v.string(),
+    displayName: v.string(),
+    description: v.string(),
+    developerName: v.string(),
+    displayIcon: v.string(),
+    bustPortrait: v.string(),
+    background: v.string(),
+    isPlayableCharacter: v.boolean(),
+    role: v.object({
+        uuid: v.string(),
+        displayName: v.string(),
+        description: v.string(),
+        displayIcon: v.string(),
+    }),
+    abilities: v.array(AbilitySchema),
+});
+
+export type AgentApiType = v.InferOutput<typeof AgentSchema>;
